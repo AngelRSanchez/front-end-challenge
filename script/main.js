107c39a1b10e4c33641c8358596e9dcb695d1f57
AngelRSanchez <angelraulsanchez3@gmail.com>
2019-09-21 00:08:17 -0400

    Transicionesm y un poco de dinamismo con js

--- script/main.js
+++ script/main.js
@@ -3,9 +3,19 @@ const e = function(e){
 }
 
 const select = e('.select');
+const cardDiv = e('.item')[1];
+
 
 for(let i = 0; i < select.length; i++){
     select[i].addEventListener('click', ()=>{
         select[i].classList.toggle('opened');
     });
 }
+
+function showCard(){
+    const card = e('#card')[0];
+    card.classList.toggle('hidden');
+    cardDiv.classList.toggle('divCard');
+}
+
+cardDiv.addEventListener('click', showCard);
